9c89cfe2b51a90046e576024fcd74cf96b27884f
jgyb89 <james.green335@du.edu>
2024-10-06 23:13:04 -0400

    Added more skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -15,18 +15,26 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
+        <SkillList src={checkMarkIcon} skill="React" />
         <SkillList src={checkMarkIcon} skill="Node" />
     </div>
     <hr />
     <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="MongoDB" />
         <SkillList src={checkMarkIcon} skill="Bootstrap" />
-        <SkillList src={checkMarkIcon} skill="Webpack" />
-        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="GitHub" />
         <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+        <SkillList src={checkMarkIcon} skill="Redux.js" />
     </div>
     <hr />
+    <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Creative Suite" />
+        <SkillList src={checkMarkIcon} skill="UI/UX" />
+        <SkillList src={checkMarkIcon} skill="Figma" />
+        <SkillList src={checkMarkIcon} skill="SEO" />
+        <SkillList src={checkMarkIcon} skill="WordPress" />
+    </div>
+    
   </section>
   );
 }
