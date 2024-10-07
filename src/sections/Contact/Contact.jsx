8534e8704165531eb4037a5ac77e9dbc4d6acc7d
jgyb89 <james.green335@du.edu>
2024-10-06 22:36:12 -0400

    Formspree integration

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,21 +4,21 @@ function Contact() {
   return (
     <section id="contact" className={styles.container}>
         <h1 className="sectionTitle">Contact</h1>
-        <form action="">
+        <form action="https://formspree.io/f/xlderwga" method="post">
             <div className="formGroup">
-                <label htmlFor="name" hidden>
+                <label for="name" htmlFor="name" hidden>
                     Name
                 </label>
                 <input type="text" name="name" id="name" placeholder="Name" required/>
             </div>
             <div className="formGroup">
-                <label htmlFor="email" hidden>
+                <label for="email" htmlFor="email" hidden>
                     Email
                 </label>
-                <input type="text" name="email" id="name" placeholder="Email" required/>
+                <input type="email" name="email" id="name" placeholder="Email" required/>
             </div>
             <div className="formGroup">
-                <label htmlFor="message" hidden>
+                <label for="message" htmlFor="message" hidden>
                     Message
                 </label>
                 <textarea name="message" id="message" placeholder="Message" required></textarea>
