5cf6ca7379b86ff38068e4b5f5c8b332b4f6241c
James Green <122503720+jgyb89@users.noreply.github.com>
2025-01-17 21:13:32 -0500

    Work projects update
    Add 3 wordpress projects
    Updated CV
    Updated all rights date

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './FooterStyles.module.css';
 function Footer() {
   return (
     <section id='footer 'className={styles.container}>
-        <p> &#169; 2024 James Green.<br/>All rights reserved.</p>
+        <p> &#169; 2025 James Green.<br/>All rights reserved.</p>
     </section>
   )
 }
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -3,11 +3,42 @@ import xora from '../../assets/xora-screen.jpg';
 import brainwave from '../../assets/brainwave-screen.jpg'
 import hoobank from '../../assets/hoobank-screen.jpg'
 import ProjectCard from '../../common/ProjectCard';
+import kotis from '../../assets/kotis-website-sample.jpg'
+import vibe from '../../assets/vibe-website-sample.jpg'
+import jag from '../../assets/jagclamp-website-sample.jpg'
 
 function Projects() {
   return  (
   <section id="projects" className={styles.container}>
-    <h1 className="sectionTitle">Projects</h1>
+    <h1 className="sectionTitle">WordPress Projects</h1>
+    <div className={styles.projectsContainer}>
+        <ProjectCard 
+        src={kotis} 
+        link={'https://go.kotisdesign.com/'}
+        h3="Kotis Design"
+        p="Swag fulfillment platform"
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+        />
+        <ProjectCard 
+        src={vibe} 
+        link={'https://www.vibebycalifornia.com/'}
+        h3="Vibe by California"
+        p="Cannabis Dispensary"
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+        />
+        <ProjectCard 
+        src={jag} 
+        link={'https://jagclamp.com/'}
+        h3="JagClamp"
+        p="Masonry Product Website"
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+        />
+        
+    </div>
+    <h1 className="sectionTitle">React UI Projects</h1>
     <div className={styles.projectsContainer}>
         <ProjectCard 
         src={xora} 
