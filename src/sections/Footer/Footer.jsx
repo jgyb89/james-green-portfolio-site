472dd6d063c864d085896fb46539c32c480dc7ae
James Green <122503720+jgyb89@users.noreply.github.com>
2026-05-10 17:04:40 -0700

    Gallery update (#3)
    * Update CCR highlights
    
    * Footer update

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,11 +1,16 @@
-import styles from './FooterStyles.module.css';
+import styles from "./FooterStyles.module.css";
 
 function Footer() {
   return (
-    <section id='footer 'className={styles.container}>
-        <p> &#169; 2025 James Green.<br/>All rights reserved.</p>
+    <section id="footer " className={styles.container}>
+      <p>
+        {" "}
+        &#169; 2026 James Green.
+        <br />
+        All rights reserved.
+      </p>
     </section>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
